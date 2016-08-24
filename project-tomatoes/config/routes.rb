085a81f7493f44d022d5b8e984503d34baeac120
Kevin M <kevin.mcgovern92@gmail.com>
2016-08-24 16:49:44 -0700

    Add home controller

--- project-tomatoes/config/routes.rb
+++ project-tomatoes/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :movies, only: [:show, :index]
   resources :users
   resources :reviews, only: [:new, :create, :show]
   resources :stars, only: [:show, :create, :new]
+  root 'home#index'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 end
