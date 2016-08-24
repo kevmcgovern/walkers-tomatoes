f07d1dcbc6c9cead6fd84c134858366d85913a60
Kevin M <kevin.mcgovern92@gmail.com>
2016-08-24 16:11:55 -0700

    Add through associations for Movie and User

--- project-tomatoes/app/models/user.rb
+++ project-tomatoes/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+	has_many :reviews
+	has_many :stars
+
+	has_many :reviewed_movies, through: :reviews, source: :movies
+	has_many :starred_movies, through: :stars, source: :movies 
 end
