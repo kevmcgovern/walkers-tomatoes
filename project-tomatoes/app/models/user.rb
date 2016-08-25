fe840a7447b1f7090a8534109b17aa00b2dfe4e5
Millie Chan <mireechan@gmail.com>
2016-08-25 11:06:02 -0700

    Update models for through associations to be source: singular_table_name

--- project-tomatoes/app/models/user.rb
+++ project-tomatoes/app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
 	has_many :reviews
 	has_many :stars
 
-	has_many :reviewed_movies, through: :reviews, source: :movies
-	has_many :starred_movies, through: :stars, source: :movies 
+	has_many :reviewed_movies, through: :reviews, source: :movie
+	has_many :starred_movies, through: :stars, source: :movie 
 end
