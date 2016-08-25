03cbbb1ebeb3e89d5745e96e29781b233c0b1516
Kevin M <kevin.mcgovern92@gmail.com>
2016-08-25 12:30:30 -0700

    Add method for user SHOW route

--- project-tomatoes/app/controllers/users_controller.rb
+++ project-tomatoes/app/controllers/users_controller.rb
@@ -2,7 +2,11 @@ class UsersController < ApplicationController
   def new
   end
 
-   def create
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
@@ -12,9 +16,9 @@ class UsersController < ApplicationController
     end
   end
 
-private
+  private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
