757bde384c783373f1b2aae46c1ba4939c56998f
Millie Chan <mireechan@gmail.com>
2016-08-25 10:38:52 -0700

    Update seeds file with movie faker data

--- project-tomatoes/db/seeds.rb
+++ project-tomatoes/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'ffaker'
+
+100.times do
+  Movie.create(
+    title: FFaker::Movie.title,
+    summary: FFaker::HipsterIpsum.paragraph,
+    cast: FFaker::Name.name(6),
+    category: FFaker::Music.genre,
+    poster_url: FFaker::Avatar.image,
+    mpaa_rating: FFaker::Movie.rating,
+  )
+end
