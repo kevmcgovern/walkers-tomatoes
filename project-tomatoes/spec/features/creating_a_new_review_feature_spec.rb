7092e327bfab66277eaff4f7d9decfea677fe1a1
Kevin M <kevin.mcgovern92@gmail.com>
2016-08-25 19:28:27 -0700

    Add tests for review submission functionality; unforeseen error prevents tests passing

--- project-tomatoes/spec/features/creating_a_new_review_feature_spec.rb
+++ project-tomatoes/spec/features/creating_a_new_review_feature_spec.rb
@@ -7,6 +7,15 @@ feature "user creates a new review" do
 			visit '/movies/15'
 			expect(page).to have_selector('#movie-reviews')
 		end
-
+		describe "user submits new review" do
+			current_user = FactoryGirl.create(:user)
+			it "saves a new review to the database" do
+				visit '/movies/15'
+				within('#movie-reviews') do
+					fill_in 'review_description', :with => "Testing, testing"
+				end
+				expect { click_button('Submit') }.to change {Review.count}.by(1)
+			end
+		end
 	end
 end
