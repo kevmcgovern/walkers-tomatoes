7e96569cf57acb8f4f218a4dc4b223575df0c633
Millie Chan <mireechan@gmail.com>
2016-08-26 15:59:39 -0700

    Clean up comments

--- project-tomatoes/app/controllers/stars_controller.rb
+++ project-tomatoes/app/controllers/stars_controller.rb
@@ -3,11 +3,6 @@
 class StarsController < ApplicationController
 
   def create 
-    p "hi" * 1000
-    p params[:id]
-    p params[:stars]
-    # if the star that was created exists, find that star through the User's star's for htis movie 
-    # else, create a new star 
     star = current_user.stars.find_by(movie_id: params[:id])
     if star 
       star.update_attributes(rating: params[:stars])
