1a4e8b6824ad4e2a99e72ec6beda0a2fee5f4638
Kevin M <kevin.mcgovern92@gmail.com>
2016-08-26 15:17:49 -0700

    Add upvote/downvote to routes file

--- project-tomatoes/config/routes.rb
+++ project-tomatoes/config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :movies, only: [:show, :index]
   resources :users
-  resources :reviews, only: [:new, :create, :show]
+  resources :reviews, only: [:new, :create, :show] do
+    member do
+      post 'upvote'
+      delete 'downvote'
+    end
+  end
   resources :stars, only: [:show, :create, :new]
   root 'home#index'
 
