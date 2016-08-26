72e3a85d704f2afc96559b4e2095b92f708b619a
Kevin M <kevin.mcgovern92@gmail.com>
2016-08-25 17:20:37 -0700

    Add test for review, fix typo in profile test

--- project-tomatoes/spec/features/creating_a_new_review_feature_spec.rb
+++ project-tomatoes/spec/features/creating_a_new_review_feature_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 feature "user creates a new review" do
-	xscenario "the user visits a movie show page and sees the review form" do
+	describe "the user visits a movie show page" do
+	movie = Movie.create(title: "Test Test Test")
+		it "shows the review form partial" do
+			visit '/movies/15'
+			expect(page).to have_selector('#movie-reviews')
+		end
+
 	end
 end
