690e9543c0bf687dd08dcb7db842c289db2d1972
Kevin M <kevin.mcgovern92@gmail.com>
2016-08-26 14:31:53 -0700

    Add method for update route

--- project-tomatoes/app/controllers/reviews_controller.rb
+++ project-tomatoes/app/controllers/reviews_controller.rb
@@ -1,17 +1,27 @@
 class ReviewsController < ApplicationController
-	def create
-		@review = Review.new(review_params)
-		if @review.save
-			p @review
-			redirect_to @review.movie
-		else
-			p @review
-			redirect_to movies_path
-		end
-	end
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      p @review
+      redirect_to @review.movie
+    else
+      p @review
+      redirect_to movies_path
+    end
+  end
 
-	private
-	def review_params
-		params.require(:review).permit(:description, :user_id, :movie_id)
-	end
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to @review.movie
+    else
+      redirect_to movies_path
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:description, :user_id, :movie_id)
+  end
+
 end
